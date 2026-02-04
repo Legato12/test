506d470444ca2ac72d878cc2a755c7c93c211b93
Legato12 <Kovrigin2010@gmail.com>
2026-02-04 09:38:50 +0800

    added tests

--- Assets/Tests/EditMode/Phase0Core_EditModeTests.cs
+++ Assets/Tests/EditMode/Phase0Core_EditModeTests.cs
@@ -20,12 +20,37 @@ public class Phase0Core_EditModeTests
     }
 
     [Test]
+    public void Bounds_LShapeNearEdge_MakesCannotPlace()
+    {
+        var grid = new GridModel(4);
+        var lShape = new[] { new Int2(0, 0), new Int2(1, 0), new Int2(0, 1), new Int2(0, 2) };
+
+        var canPlace = grid.CanPlace(new Int2(3, 2), lShape, out _);
+
+        Assert.False(canPlace);
+    }
+
+    [Test]
+    public void Rotation_NearEdge_RotatedOutOfBounds_MakesCannotPlace()
+    {
+        var grid = new GridModel(4);
+        var baseCells = new[] { new Int2(0, 0), new Int2(1, 0), new Int2(1, 1), new Int2(2, 1) };
+        var rotated = ShapeRotation.GetRotated(baseCells, Int2.zero, rotationIndexCW: 1);
+
+        var canPlace = grid.CanPlace(new Int2(1, 0), rotated, out _);
+
+        Assert.False(canPlace);
+    }
+
+    [Test]
     public void BlockedCell_MakesCannotPlace()
     {
         var grid = new GridModel(4);
         grid.SetBlocked(new[] { new Int2(1, 1) });
 
-        var canPlace = grid.CanPlace(new Int2(0, 0), new[] { new Int2(1, 1) }, out _);
+        var shape = new[] { new Int2(0, 0), new Int2(1, 0), new Int2(0, 1) };
+
+        var canPlace = grid.CanPlace(new Int2(1, 1), shape, out _);
 
         Assert.False(canPlace);
     }
@@ -36,7 +61,9 @@ public class Phase0Core_EditModeTests
         var grid = new GridModel(4);
         grid.AddOccupied(new[] { new Int2(2, 0) });
 
-        var canPlace = grid.CanPlace(new Int2(2, 0), new[] { new Int2(0, 0) }, out _);
+        var shape = new[] { new Int2(0, 0), new Int2(1, 0), new Int2(0, 1) };
+
+        var canPlace = grid.CanPlace(new Int2(2, 0), shape, out _);
 
         Assert.False(canPlace);
     }
